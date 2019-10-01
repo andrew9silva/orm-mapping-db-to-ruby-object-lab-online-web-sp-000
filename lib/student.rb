5970757d6c99a094d6e6302b6224855d3677f920
Andrew Silva <andrewsilva9@gmail.com>
2019-10-01 02:36:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -121,7 +121,7 @@ class Student
       end.first
     end
 
-    def self.all_students_in_grade_X
+    def self.all_students_in_grade_X(num)
 
       sql = <<-SQL
       SELECT *
@@ -129,7 +129,7 @@ class Student
       WHERE grade = ?
       SQL
 
-      DB[:conn].execute(sql).map do |row|
+      DB[:conn].execute(sql, num).map do |row|
         self.new_from_db(row)
       end
     end
